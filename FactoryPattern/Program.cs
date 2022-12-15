a43787156a992e1ee9d88ba8c1742fdc98c44bea
Zachary Haggerty <zhaggerty24@gmail.com>
2022-12-14 21:56:24 -0500

    initial commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace FactoryPattern
+﻿using System.Data.SqlTypes;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What type of vehicle would you like to make? Car or Motorcycle");
+            string userInput = Console.ReadLine();
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(userInput);
+            vehicle.Drive();
+            Console.ReadLine();
+
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string userInput)
+        {
+            switch (userInput.ToLower())
+            {
+                case "car":
+                    return new Car();
+                    
+                case "Motorcycle":
+                    return new Motorcycle();
+                    
+               default:
+                    return new Car();
+            }
+        }
+    }
+}
